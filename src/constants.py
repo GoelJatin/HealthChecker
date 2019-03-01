02b5cda8114329a85b77a63e56a5706bdfb808fd
Jatin Goel <jatingoel1037@hotmail.com>
2019-03-01 19:12:56 +0530

    Add Constants file

---
+++ src/constants.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Jatin Goel
+"""
+
+import os
+
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+PEM_DIR = os.path.join(ROOT_DIR, 'keys')
+
+PRIVATE_KEY_PATH = os.path.join(PEM_DIR, 'private.pem')
+PUBLIC_KEY_PATH = os.path.join(PEM_DIR, 'public.pem')
