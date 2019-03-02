3046aaa92b182cff73c67134e5d54a617c9b7611
GoelJatin <jatingoel1037@hotmail.com>
2019-03-02 15:03:00 +0530

    Added doc-strings and removed unused imports

--- src/database/resource_model.py
+++ src/database/resource_model.py
@@ -3,15 +3,11 @@
 @author: Jatin Goel
 """
 
-import json
-import uuid
-
 from base64 import b64encode
 
 from flask_sqlalchemy import SQLAlchemy
 
-from ..settings import app
-from ..encryption.encrypt import encrypt
+from ..settings import APP
 from ..encryption.encryption import Encryption
 from ..exception import (
     NoSuchResourceException,
@@ -20,24 +16,54 @@ from ..exception import (
 )
 
 
-DB = SQLAlchemy(app)
+DB = SQLAlchemy(APP)
 
 
 class Resource(DB.Model):
+    """Table for storing Resource related information."""
+
     __tablename__ = 'Resource'
 
     id = DB.Column(DB.Integer, primary_key=True)
     hostname = DB.Column(DB.String(50), nullable=False, unique=True)
     username = DB.Column(DB.String(32), nullable=False)
     password = DB.Column(DB.String(100), nullable=False)
+    interval = DB.Column(DB.Integer, nullable=False)
 
     def __repr__(self):
-        return f'Resource: {self.hostname}, for User: {self.username}'
+        return (
+            f'Resource: {self.hostname}, '
+            f'for User: {self.username}, '
+            f'with Interval: {self.interval}'
+        )
 
     def get_all_resources():
+        """Returns the list of all the resources added to the Table."""
         return Resource.query.all()
 
-    def add_resource(_hostname, _username, _password):
+    def add_resource(_hostname, _username, _password, _interval=60):
+        """Add a new resource to the Table, if it does not exists.
+
+            Args:
+                _hostname   (str):  hostname / ip address of the resource
+
+                _username   (str):  username of the resource
+
+                _password   (str):  password of the resource
+
+                _interval   (int):  interval value to be used b/w consecutive pings (in seconds)
+
+                    default:    60
+
+            Returns:
+                None:   if the resource is added successfully
+
+            Raises:
+                ResourceAlreadyExistsException:     if resource already exists with given hostname
+
+                EncryptionFailedException:          if encryption failed for the given password
+
+        """
         try:
             Resource.get_resource(_hostname)
             raise ResourceAlreadyExistsException('Please give a unique hostname')
@@ -47,18 +73,26 @@ class Resource(DB.Model):
             try:
                 data = encryption.encrypt(_password)
             except ValueError as error:
-                raise EncryptionFailedException(f'Failed to encrypt the message due to the error: [{error}]')
+                raise EncryptionFailedException(
+                    f'Failed to encrypt the message due to the error: [{error}]'
+                )
 
-            for key in data.keys():
+            for key in data:
                 data[key] = b64encode(data[key])
 
             _password = b64encode(str(data).encode()).decode()
 
-            new_resource = Resource(hostname=_hostname, username=_username, password=_password)
+            new_resource = Resource(
+                hostname=_hostname,
+                username=_username,
+                password=_password,
+                interval=_interval
+            )
             DB.session.add(new_resource)
             DB.session.commit()
 
     def get_resource(_hostname):
+        """Returns a SQLAlchemy object of the row (resource) that matches the given hostname."""
         resource = Resource.query.filter_by(hostname=_hostname).first()
 
         if resource:
@@ -67,6 +101,7 @@ class Resource(DB.Model):
         raise NoSuchResourceException('No resource exists with the given hostname')
 
     def delete_resource(_hostname, _username, _password):
+        """Deletes the resource with the given hostname, if username and password also matches."""
         resource = Resource.get_resource(_hostname)
 
         if resource and _username == resource.username and _password == resource.password:
--- src/database/user_model.py
+++ src/database/user_model.py
@@ -7,15 +7,17 @@ import uuid
 
 from flask_sqlalchemy import SQLAlchemy
 
-from ..settings import app
+from ..settings import APP
 from ..encryption.encrypt import encrypt
 from ..exception import NoSuchUserException, UserAlreadyExistsException
 
 
-DB = SQLAlchemy(app)
+DB = SQLAlchemy(APP)
 
 
 class User(DB.Model):
+    """Table for storing User related information."""
+
     __tablename__ = 'Users'
 
     id = DB.Column(DB.Integer, primary_key=True)
@@ -27,9 +29,24 @@ class User(DB.Model):
         return f'User: {self.username}'
 
     def get_all_users():
+        """Returns the list of all the users added to the Table."""
         return User.query.all()
 
     def add_user(_username, _password):
+        """Add a new user to the Table, if it does not exists.
+
+            Args:
+                _username   (str):  username of the new user
+
+                _password   (str):  password of the new user
+
+            Returns:
+                None:   if the user is added successfully
+
+            Raises:
+                UserAlreadyExistsException:     if a user already exists with the given username
+
+        """
         try:
             User.get_user(_username)
             raise UserAlreadyExistsException('Please give a unique username')
@@ -46,6 +63,7 @@ class User(DB.Model):
             DB.session.commit()
 
     def get_user(_username):
+        """Returns a SQLAlchemy object of the row (user) that matches the given username."""
         user = User.query.filter_by(username=_username).first()
 
         if user:
@@ -54,6 +72,7 @@ class User(DB.Model):
         raise NoSuchUserException('No username exists with the given name')
 
     def delete_user(_username, _password):
+        """Deletes the user with the given username, if password also matches."""
         user = User.get_user(_username)
 
         if user and encrypt(user.salt, _password) == user.password:
@@ -64,6 +83,7 @@ class User(DB.Model):
         return False
 
     def authenticate(_username, _password):
+        """Checks if a user with the given username and password exists in the table or not."""
         user = User.query.filter_by(username=_username).first()
 
         if user and encrypt(user.salt, _password) == user.password:
