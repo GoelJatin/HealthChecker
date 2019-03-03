f22512d6f0ecacff81987b2b380d18a0d777986e
Jatin Goel <jatingoel1037@hotmail.com>
2019-03-03 20:35:51 +0530

    Health Aggregator Fixes

--- src/health_aggregator.py
+++ src/health_aggregator.py
@@ -3,11 +3,14 @@
 @author: Jatin Goel
 """
 
+import time
+
 from threading import Thread
 
 from exception import InvalidOperationException
 from database.resource_model import Resource as DB_Resource
 from resource_ops.resource import Resource as Client_Resource
+from resource_ops.unix_resource import UnixResource
 
 
 class HealthAggregator:
@@ -21,16 +24,22 @@ class HealthAggregator:
 
     def _initialize(self, resource):
         """Initialize the Resource object for the given entity, and set its status."""
-        self.resources[resource.hostname] = {
-            'resource': Client_Resource(
-                resource.hostname,
-                resource.username,
-                resource._decrypt_password(resource.password)
-            ),
-            'interval': resource.interval
-        }
+        try:
+            self.resources[resource.hostname] = {
+                'resource': Client_Resource(
+                    resource.hostname,
+                    resource.username,
+                    resource._decrypt_password(resource.password)
+                ),
+                'interval': resource.interval
+            }
 
-        self.resource_state[resource.hostname] = self.resources[resource.hostname].is_healthy()
+            self.resource_state[resource.hostname] = self.resources[resource.hostname]['resource'].is_healthy()
+        except Exception:
+            # in case the resource connection failed
+            # since the resource is not added to the **resources** dict
+            # it shall be tried again during syncronize
+            self.resource_state[resource.hostname] = False
 
     def synchronize(self, operation='add'):
         """Synchronize the list of active resources and their status."""
@@ -51,22 +60,54 @@ class HealthAggregator:
         else:
             raise InvalidOperationException()
 
-    def worker(self, resource):
-        """Worker block to process each resource.
-
-            Args:
-                resource    (str):  hostname of the resource to be processed
-
-        """
-        self.resource_state[resource] = self.resources[resource]['resource'].is_healthy()
-        time.sleep(self.resources[resource]['interval'])
-
     def start(self):
         """Start thread for each resource to get the status."""
         for resource in self.resources:
-            thread = Thread(target=self.worker, args=(resource), daemon=True)
+            thread = Thread(target=worker, args=(self, resource,), daemon=True)
             thread.start()
+            # self.worker(resource)
 
     def is_healthy(self):
         """Checks and returns if all resources are in a healthy state or not."""
         return all(self.resource_state.values())
+
+    def cleanup(self):
+        """Disconnect from all the resources."""
+        for resource in self.resources:
+            self.resources[resource]['resource'].disconnect()
+
+        del self.resources
+        del self.resource_state
+
+
+def worker(cls_object, resource):
+    """Worker block to process each resource.
+
+        Args:
+            cls_object  (object):   instance of the HealthAggregator class
+
+            resource    (str):      hostname of the resource to be processed
+
+    """
+    while True:
+        synchronize = False
+
+        try:
+            health = cls_object.resources[resource]['resource'].is_healthy()
+            cls_object.resource_state[resource] = health
+
+            interval = cls_object.resources[resource]['interval']
+
+            if health is False and isinstance(cls_object.resources[resource]['resource'], UnixResource):
+                # in case of UNIX machine, SSH tunnel is closed
+                # hence we need to delete this object, and create new connection
+                del cls_object.resources[resource]
+                synchronize = True
+        except KeyError:
+            cls_object.resource_state[resource] = False
+            synchronize = True
+
+        if synchronize:
+            cls_object.synchronize()
+
+        time.sleep(interval)
